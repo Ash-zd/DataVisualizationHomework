749fb2ffe135098ab1def8b635159473a8b84e02
zd <ash_zd@qq.com>
2019-01-03 13:58:24 +0800

    update web_backend

--- web_backend/tutorial/quickstart/views.py
+++ web_backend/tutorial/quickstart/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from quickstart.serializers import UserSerializer, GroupSerializer
